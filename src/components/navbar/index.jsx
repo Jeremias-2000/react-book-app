2c93f3d8dc218ec4e4b7888090937c8e4651e60e
jeremias-2000 <jdossantospinheiro20@gmail.com>
2021-08-09 18:51:45 -0300

    criando navbar e input de pesquisa

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBarStyles.css'
+const NavBar= () => {
+    return (
+        <div className="navBar">
+            <ul>
+                <li>Favoritos</li>
+                <li>Categorias</li>
+                <li>Carrinho</li>
+                <li>Login</li>
+                <li>Cadastre-se</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter,Route,Switch}  from 'react-router-dom';
-
+import SignIn from './pages/SignIn'
 
 import Home from './pages/Home';
+
 const Routes = () => (
     <BrowserRouter>
         <Switch> 
             <Route path= "/" exact component = {Home}/>
+            <Route path= "/signin" exact component = {SignIn}/>
+            
         </Switch>
     </BrowserRouter>
 )
